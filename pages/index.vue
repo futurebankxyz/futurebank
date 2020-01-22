6e0039a52d9a51a4f85ab8e5c590bc9991c9130f
Developer Fred <developerfred@protonmail.ch>
2020-01-22 14:07:26 -0300

    [WIP] - Future Bank Landing Page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1 class="futurebank">Future Bank</h1>
+  </div>
+</template>
+<style>
+body {
+  @apply bg-pink-600;
+}
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+.futurebank {
+  text-transform: uppercase;
+  font-family: Alata;
+  color: #fff;
+}
+</style>
