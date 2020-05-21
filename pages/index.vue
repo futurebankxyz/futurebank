8bedcceb5f6e2dc0130ad517a2f654b2a1b637e3
codingsh <developerfred@protonmail.ch>
2020-05-21 19:31:07 -0300

    eslint fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
-    <iframe class="airtable-embed" src="https://airtable.com/embed/shrZEyITJgTotvX1t?backgroundColor=pink" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+    <iframe
+      class="airtable-embed"
+      src="https://airtable.com/embed/shrZEyITJgTotvX1t?backgroundColor=pink"
+      frameborder="0"
+      onmousewheel=""
+      width="100%"
+      height="533"
+      style="background: transparent; border: 1px solid #ccc;"
+    ></iframe>
   </div>
 </template>
 <style>
